8a923e70b8479b7a63d882021f68bebc6e6dc6cd
Екатерина Петрова <katedesu1@yandex.ru>
2025-12-19 01:47:47 +0500

    FormProducts изменение отображения таблицы

--- ShoesProject/FormProducts.cs
+++ ShoesProject/FormProducts.cs
@@ -55,6 +55,7 @@ namespace ShoesProject
                         .Include(i => i.Manufacturer)
                         .Include(i => i.Supplier)
                         .Include(i => i.Measure)
+                        .Include(i => i.ProductType)
                         .ToList();
 
                     dgvProducts.SuspendLayout();
@@ -74,6 +75,9 @@ namespace ShoesProject
 
                         ApplyRowStyles(row, product);
                     }
+
+                    dgvProducts.ResumeLayout();
+                    dgvProducts.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
                 }
             }
             catch (Exception ex)
